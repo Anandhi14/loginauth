829bc2fa5fd511aa0a7262a427276139faf7c6b2
sharmila37 <143999413+sharmila37@users.noreply.github.com>
2023-10-05 16:11:07 +0530

    cccc

--- src/app/firebaseservice.service.ts
+++ src/app/firebaseservice.service.ts
@@ -26,7 +26,7 @@ export class FirebaseserviceService {
     });
   }
   logout():Promise<void>{   
-    //ppjj
+    //ppjjjjjjj
    return  this.firebaseauth.signOut();
     localStorage.removeItem('user')
   }
